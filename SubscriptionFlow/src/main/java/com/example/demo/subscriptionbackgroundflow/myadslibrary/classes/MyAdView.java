457966ab325fec2ec90d4e2f34243f013f04da60
Yagnik <akshay3.vasundhara@gmail.com>
2023-04-24 10:01:48 +0530

    Subscription Flow With Ads

--- SubscriptionFlow/src/main/java/com/example/demo/subscriptionbackgroundflow/myadslibrary/classes/MyAdView.java
+++ SubscriptionFlow/src/main/java/com/example/demo/subscriptionbackgroundflow/myadslibrary/classes/MyAdView.java
@@ -3,18 +3,14 @@ package com.example.demo.subscriptionbackgroundflow.myadslibrary.classes;
 import android.content.Context;
 import android.content.Intent;
 
-import com.example.demo.subscriptionbackgroundflow.myadslibrary.ui.MoreAppActivity;
-import com.example.demo.subscriptionbackgroundflow.myadslibrary.ui.MoreAppActivityNew;
-import com.example.demo.subscriptionbackgroundflow.myadslibrary.ui.NewMoreAppActivity;
-
 public class MyAdView {
 
     public static MyAdView getInstance(){
         return new MyAdView();
     }
 
-    public Intent getMoreAppIntent(Context context){
-        Intent intent = new Intent(context, NewMoreAppActivity.class);
-        return intent;
-    }
+//    public Intent getMoreAppIntent(Context context){
+//        Intent intent = new Intent(context, NewMoreAppActivity.class);
+//        return intent;
+//    }
 }
